3604869209c5cc55ba4348ab6ecb16af6720f251
JefferNg <jefferng11@gmail.com>
2025-02-08 20:25:42 -0800

    implemented anims and movement

--- Calypso/Assets/Scripts/PlayerMovement.cs
+++ Calypso/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private Rigidbody2D rb;
     private float movementX;
     [SerializeField] float speed;
+    public Animator animator;
+    private bool facingRight = true;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,15 +20,39 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = new Vector2(movementX * speed, rb.linearVelocityY);
+        if (rb.linearVelocityY == 0)
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        if (movementX > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (movementX < 0 && facingRight)
+        {
+            Flip();
+        }
     }
 
     void OnXMove(InputValue inputValue)
     {
         movementX = inputValue.Get<float>();
+        animator.SetFloat("Speed", Mathf.Abs(movementX));
     }
 
     void OnJump()
     {
-        Debug.Log("Hello!");
+        animator.SetBool("Jump", true);
+        rb.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
