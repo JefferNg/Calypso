8fab20de2dfe041732b47fb7ef6c77320b55f3e1
Christopher Grigorian <christopher.grigorian@gmail.com>
2025-02-10 22:32:55 -0800

    Skeleton & Weapon Burst Interaction
    Created a skeleton enemy that can walk between two points. On left click, Calypso can shoot a beam from her orb, which will destroy skeletons hit by the beam.

---
+++ Assets/Scripts/WeaponBurst.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class WeaponBurst : MonoBehaviour
+{
+    private Collider2D colliderWeapon;
+    public Animator animator;
+    private bool inAttack = false;
+
+    void Start()
+    {
+        colliderWeapon = GetComponent<Collider2D>();
+        colliderWeapon.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !inAttack)
+        {
+            StartCoroutine(PerformAttack());
+        }
+    }
+
+    private IEnumerator PerformAttack()
+    {
+        inAttack = true;
+
+        animator.SetBool("AttackOccured", true);
+
+        yield return new WaitForSeconds(0.5f);
+        colliderWeapon.enabled = true;
+
+        yield return new WaitForSeconds(0.5f);
+
+        colliderWeapon.enabled = false;
+        animator.SetBool("AttackOccured", false);
+        inAttack = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
