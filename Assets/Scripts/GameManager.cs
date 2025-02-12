4f900a9e475e51f0701f884e4e9792db032bc21c
JefferNg <jefferng11@gmail.com>
2025-02-11 23:26:58 -0800

    implemented in game ui

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject ui;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMenu()
+    {
+        ui.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        ui.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
